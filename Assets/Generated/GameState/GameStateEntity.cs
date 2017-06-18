b94d3e0cd618c5da257326b38c6442082b7302f3
René Löscher <ReneLoescher@gmx.net>
2017-06-18 19:35:15 +0200

    generated new

--- Assets/Generated/GameState/GameStateEntity.cs
+++ Assets/Generated/GameState/GameStateEntity.cs
@@ -6,5 +6,5 @@
 //     the code is regenerated.
 // </auto-generated>
 //------------------------------------------------------------------------------
-public sealed partial class GameEntity : Entitas.Entity {
+public sealed partial class GameStateEntity : Entitas.Entity {
 }
